c93a939ed038e9adb8cbc2340524ad120dc3dda1
jaehyok <freejh21@gmail.com>
2016-07-12 23:46:10 +0900

    1st commit

---
+++ helloRedis/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  helloRedis
+//
+//  Created by jaehyok on 2016. 7. 12..
+//  Copyright © 2016년 jaehyok. All rights reserved.
+//
+
+#include <iostream>
+#include "hiredis.h"
+
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	
+	return 0;
+}
