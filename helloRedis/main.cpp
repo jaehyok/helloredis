7f299c8c8cae1d5b3f3a688b1472d21c26ffc898
jaehyok <freejh21@gmail.com>
2016-07-12 23:52:16 +0900

    using hiredis functions.

--- helloRedis/main.cpp
+++ helloRedis/main.cpp
@@ -14,5 +14,29 @@ int main(int argc, const char * argv[]) {
 	// insert code here...
 	std::cout << "Hello, World!\n";
 	
+	redisContext *c = NULL;
+	redisReply *reply = NULL;
+	struct timeval timeout = { 1, 500000 }; // 1.5 seconds
+	c = redisConnectWithTimeout("127.0.0.1", 6379, timeout);
+	if (c == NULL || c->err) {
+		if (c) {
+			printf("Connection error: %s\n", c->errstr);
+			redisFree(c);
+		} else {
+			printf("Connection error: can't allocate redis context\n");
+		}
+		exit(1);
+	}
+	
+	/* PING server */
+	reply = (redisReply *)redisCommand(c,"PING");
+	printf("PING: %s\n", reply->str);
+	freeReplyObject(reply);
+	
+	
+	
+	/* Disconnects and frees the context */
+	redisFree(c);
+	
 	return 0;
 }
